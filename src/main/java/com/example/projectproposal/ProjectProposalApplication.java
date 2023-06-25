68a4a724b5c1729162c04d71623b93f866716e00
kevin-sys <ksinasignargomez@unicesar.edu.co>
2023-06-24 23:02:44 -0500

    First Commit

---
+++ src/main/java/com/example/projectproposal/ProjectProposalApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectproposal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectProposalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectProposalApplication.class, args);
+	}
+
+}
